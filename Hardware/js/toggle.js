58cefa12dc074ccca5a162d0dc76bf6085c4f538
iamharveyS <harvey199826@gmail.com>
2022-08-17 17:29:54 +0800

    G

---
+++ Hardware/js/toggle.js
@@ -0,0 +1,8 @@
+function toggleMenu(){
+    document.getElementById("sally").classList.toggle("open");
+    document.getElementById("hamburgerbtn").classList.toggle("open");
+}
+
+const x = document.getElementById('hamburgerbtn');
+x.onlick = toggleMenu; 
+
